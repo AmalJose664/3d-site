912ad43d607db371dc7050f9c6dd7b411cf95ab9
AmalJose <amal446446@gmai.com>
2025-05-02 01:27:32 +0530

    Modifications

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,12 @@
-
+import Hero from "./sections/Hero"
+import ShowcaseSection from "./sections/ShowcaseSection"
 
 const App = () => {
   return (
-	<main>
-		<div>
-			<h2 className="text-3xl">Welcome to portfolio</h2>
-		</div>
-	</main>
+	<>
+		<Hero/>
+		<ShowcaseSection/>
+	</>
   )
 }
 
---
+++ src/components/Button.jsx
@@ -0,0 +1,25 @@
+
+const Button = ({ text, className, id}) => {
+  return (
+	<a href="" className={`${className ?? ''} cta-wrapper`} onClick={(e)=>{
+		e.preventDefault()
+		const target = document.getElementById('counter')
+		if(target && id){
+			const offset = window.innerHeight * 0.15;
+
+			const top = target.getBoundingClientRect().top + window.scrollY - offset
+			window.scrollTo({top, behavior:'smooth'})
+		}
+	}}>
+		<div className="cta-button group">
+			<div className="bg-circle"/>
+			<p className="text">{text}</p>
+			<div className="arrow-wrapper">
+				<img src="/public/images/arrow-down.svg" alt="arrrow-down" />
+			</div>
+		</div>
+	</a>
+  )
+}
+
+export default Button
